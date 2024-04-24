84b6534e1e1f2aa658b0084e2161e5e96a8a2b7a
Khalidgithub2020331007 <107164227+Khalidgithub2020331007@users.noreply.github.com>
2024-04-24 22:31:09 +0600

    Update Stack Widget.dart

--- Stack Widget.dart
+++ Stack Widget.dart
@@ -1,1 +1,49 @@
 
+
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MaterialApp(
+    title: "Stack Example",
+    home:Scaffold(
+      appBar: AppBar(title: Text("Stack Example")),
+      body:MyApp(),
+    ),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Stack(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        // alignment: Alignment.center,
+    
+        children: [
+          Container(
+            width: 200,
+            height: 200,
+            color: Colors.red,
+          ), Container(
+            width: 100,
+            height: 100,
+            color: Colors.green,
+          ),
+          Positioned(
+              bottom: -25,
+              right:0,
+              child:Container(
+                width: 50,
+                height: 50,
+                color: Colors.blue,
+              ),)
+        ],
+      ),
+    );
+  }
+}
+
