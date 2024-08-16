540ca41de3f584e5fb1d2568ffbdf166dee24da6
Sheikh Khalid Azad <107164227+Khalidgithub2020331007@users.noreply.github.com>
2024-08-16 19:39:40 +0600

    Create SingleChildScrollView.dart

---
+++ SingleChildScrollView.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Column'),
+          ),
+          body: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                Container(
+                  width: 300,
+                  height: 400,
+                  color: Colors.black,
+                ),
+                const SizedBox(
+                  height: 20,
+                  width: 20,
+                ),
+                Container(
+                  width: 300,
+                  height: 400,
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
