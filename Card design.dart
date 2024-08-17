3eb9d10a2128aee7aa223665f57d633202585fe6
Sheikh Khalid Azad <107164227+Khalidgithub2020331007@users.noreply.github.com>
2024-08-17 13:21:23 +0600

    Create Card design.dart

---
+++ Card design.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          body: Center(
+        child: Card(
+          elevation: 20,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(50)),
+          ),
+          child: Container(
+            width: 300,
+            height: 300,
+          ),
+        ),
+      )),
+    );
+  }
+}
